f48f6b22dd2c3987eaa9edb1ddd516be17402a70
rain <45768231+cyber-land@users.noreply.github.com>
2022-03-21 08:01:57 +0100

    init client & server

---
+++ client_uni/src/main.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+import { Ctx } from './context.jsx'
+//import server_addr from './config'
+
+const Main = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
